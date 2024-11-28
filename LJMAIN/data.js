7a61ab254804c1767dd40bcae0f16bef24a120e4
myusernameisit <a0906528653@gmail.com>
2024-11-28 11:46:38 +0800

    AD

---
+++ LJMAIN/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "李氏古宅",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.9026154730777005,
+          "pitch": -0.21603828449044116,
+          "title": "李氏古宅",
+          "text": "李氏古宅的建築主體"
+        }
+      ]
+    }
+  ],
+  "name": "LJMAIN",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
