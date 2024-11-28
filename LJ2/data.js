080f996c2ea39661be36be6d79b68dbcae97a318
myusernameisit <a0906528653@gmail.com>
2024-11-28 11:43:52 +0800

    ADD

---
+++ LJ2/data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "李友邦與其妻子紀念碑",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1712,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7211290386351799,
+          "pitch": 0.0008209053951500067,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.1462112433088656,
+          "pitch": 0.033690740475439185,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8448352176491056,
+          "pitch": -0.109848346263405,
+          "title": "李友邦與其妻子紀念碑",
+          "text": "紀念李友邦與其妻子"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "虎爺抓周",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1300,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6206738310536686,
+          "pitch": 0.18323123589883217,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.9724493273907369,
+          "pitch": 0.08103129692810995,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5026715357690215,
+          "pitch": 0.05167574741161296,
+          "title": "虎爺抓周區",
+          "text": "人們有小孩時可能會來這抓周"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "廚房",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1300,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.800608100045247,
+          "pitch": 0.28343457558856144,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.127012856313769,
+          "pitch": 0.2641131153940286,
+          "rotation": 4.71238898038469,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.39794380479208513,
+          "pitch": 0.1016177566425398,
+          "title": "李宅廚房",
+          "text": "李家以前的廚房，有蒸籠、米缸等"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "臼",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1300,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2540302513464026,
+          "pitch": 0.07105874611859164,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.8030008055710809,
+          "pitch": 0.14355472475844167,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.942897793795126,
+          "pitch": 0.35451756914356736,
+          "title": "臼",
+          "text": "李家用來磨箇中東西的臼"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "牆壁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1300,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0001595638351333,
+          "pitch": 0.11599089402294283,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": 3.016995455773287,
+          "pitch": 0.15754236814259315,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4490302063147702,
+          "pitch": 0.009514701326679642,
+          "title": "李宅的牆",
+          "text": "介紹李氏古宅的牆體"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
