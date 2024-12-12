bd8c11fd5a05b603a354d13f061a32c2f7ec5af1
myusernameisit <a0906528653@gmail.com>
2024-12-12 10:42:31 +0800

    333

--- LJ2/data.js
+++ LJ2/data.js
@@ -260,7 +260,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
